0799c04982b4eed51108621782ebfedd560fd96f
Josh Maguire <jmaguire@pcs-publishing.com>
2023-05-26 18:22:35 +0100

    ensure profile page cannot be viewed until a profile has been created.

--- routes/profile.tsx
+++ routes/profile.tsx
@@ -1,7 +1,7 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { ServerState } from "routes/_middleware.ts";
 import { Layout } from "components/index.ts";
-import { getUserProfile } from "../utils/user/profile.tsx";
+import { getUserProfile, userHasProfile } from "../utils/user/profile.tsx";
 
 interface Profile {
   id: string;
@@ -20,10 +20,18 @@ interface Props {
 
 export const handler: Handlers = {
   async GET(req, ctx) {
+    const url = new URL(req.url);
     const state = ctx.state as ServerState;
     let profile = null;
+    let hasProfile = false;
 
     if (state.user) {
+      hasProfile = await userHasProfile(state.user?.id);
+
+      if (!hasProfile) {
+        return Response.redirect(url.origin + "/create-profile", 303);
+      }
+
       profile = await getUserProfile(state.user?.id);
     }
 
